f3953097c3583598b80b401825258b25d4d92329
khinezinthin <khinezinthinwk1@gmail.com>
2023-08-16 11:24:15 +0630

    path profile file name edit

--- src/Routes/Path.jsx
+++ src/Routes/Path.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import Dashboard from "../Pages/Dashboard";
 import Login from "../Components/Login/Login";
 import Overview from "../Components/User/Overview";
+import AdminProfile from "../Components/Admin/AdminProfile";
 import AdminProfileEdit from "../Components/Admin/AdminProfileEdit";
 import CreateUser from "../Components/User/CreateUser";
 
@@ -14,6 +15,7 @@ const Path = () => {
         <Route path="/login" element={<Login/>} />
         <Route path="/user/overview" element={<Overview/>} />
         <Route path="/user/create" element={<CreateUser/>} />
+        <Route path="/profile/myAccount" element={<AdminProfile/>} />
         <Route path="/profile/edit" element={<AdminProfileEdit/>} />
       </Routes>
     </>
