edce8eaf39bdd22c37be0b2fe34ae5c124b3e8b4
khinezinthin <khinezinthinwk1@gmail.com>
2023-08-20 20:18:22 +0630

    api

--- src/Components/Admin/PersonalForm.jsx
+++ src/Components/Admin/PersonalForm.jsx
@@ -1,14 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useEditProfileMutation } from '../../Redux/API/adminApi';
+import Cookies from 'js-cookie';
+import Swal from 'sweetalert2';
+import "../User/successAlert.css"
 
 const PersonalForm = () => {
+    const token = Cookies.get("token");
+    const [editProfile] = useEditProfileMutation();
+    const [name,setName] = useState("");
+    const [phone,setPhone] = useState("");
+    const [date_of_birth,setDOB] = useState("");
+    const [gender,setGender] = useState("");
+    const [address,setAddress] = useState("");
+    const [user_photo,setPhoto] = useState("");
+
+    const editPpCancelHandler = () => {
+        setName(""),
+        setPhone(""),
+        setDOB(""),
+        setGender(null),
+        setAddress(""),
+        setPhone("")
+    }
+
+    const editProfileHandler = async(e) => {
+        try{
+            e.preventDefault()
+            const newPpData = {name, phone, date_of_birth, gender, address, user_photo}
+            const data = await editProfile({token,newData:newPpData})
+            console.log(data);
+            if(data?.message){
+                editPpCancelHandler()
+                Swal.fire({
+                    customClass : {
+                      title: 'swal2-title'
+                    },
+                    title: "update user successfully",
+                    icon: "success",
+                    confirmButtonText: "OK",
+                    // showCloseButton: true,
+                    width: 400,
+                    background: "#161618",
+                  })
+            }
+        }catch(error){
+            console.log(error);
+        }
+    }
   return (
     <div>
-        <form className="p-10 flex flex-col gap-6 w-full">
+        <form onSubmit={editProfileHandler} className="p-10 flex flex-col gap-6 w-full">
                     <div className="flex justify-between gap-5">
                     <label className="text-[#FFFFFF] font-medium tracking-wider">
                         Name
                     </label>
-                    <input
+                    <input onChange={(e)=>setName(e.target.value)}
+                        value={name}
                         type="text"
                         placeholder="Your Name"
                         className="input w-[70%]"
@@ -19,14 +66,14 @@ const PersonalForm = () => {
                     <label className="text-[#FFFFFF] font-medium tracking-wider">
                         Phone
                     </label>
-                    <input type="text" className="input w-[70%]" />
+                    <input onChange={(e)=>setPhone(e.target.value)} value={phone} type="text" className="input w-[70%]" />
                     </div>
 
                     <div className="flex justify-between gap-5">
                     <label className="text-[#FFFFFF] font-medium tracking-wider">
                         Date Of Birth
                     </label>
-                    <input type="text" className="input w-[70%]" />
+                    <input value={date_of_birth} placeholder='19/12/2002' onChange={(e)=>setDOB(e.target.value)} type="text" className="input w-[70%]" />
                     </div>
 
                     <div className="flex justify-between gap-5">
@@ -34,14 +81,16 @@ const PersonalForm = () => {
                         Gender
                     </label>
                     <div className=" flex gap-4 items-center w-[70%] text-[#FFFFFF] font-medium tracking-wider">
-                        <input
+                        <input onChange={(e)=>setGender(e.target.value)}
                         type="radio"
                         name="gender"
+                        value={"male"}
                         // checked
                         style={{ width: "18px", height: "18px" }}
                         />
                         Male
-                        <input
+                        <input onChange={(e)=>setGender(e.target.value)}
+                        value={"female"}
                         type="radio"
                         name="gender"
                         style={{ width: "18px", height: "18px" }}
@@ -54,7 +103,8 @@ const PersonalForm = () => {
                     <label className="text-[#FFFFFF] font-medium tracking-wider">
                         Address
                     </label>
-                    <textarea
+                    <textarea onChange={(e)=>setAddress(e.target.value)}
+                        value={address}
                         placeholder="Your Address..."
                         cols="30"
                         rows="4"
@@ -64,7 +114,7 @@ const PersonalForm = () => {
 
                     {/* btn  */}
                     <div className=" flex items-center gap-5 mt-8">
-                    <button className="btn3 flex gap-2 items-center border border-[#7E7F80] text-[#7E7F80]">
+                    <button onClick={editPpCancelHandler} className="btn3 flex gap-2 items-center border border-[#7E7F80] text-[#7E7F80]">
                         Cancle
                     </button>
                     <button className="btn flex gap-2 items-center border border-[#3f4245]">
