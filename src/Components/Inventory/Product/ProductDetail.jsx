ce1dbfe870c03cedbea54ba8bb3d47c3bb8ec9eb
PhyoWunnaLin <phyowunnalin1214@gmail.com>
2023-08-21 22:46:21 +0630

    product detail

--- src/Components/Inventory/Product/ProductDetail.jsx
+++ src/Components/Inventory/Product/ProductDetail.jsx
@@ -5,8 +5,17 @@ import { CiShop } from 'react-icons/ci'
 import Banner2 from '../../Banner/Banner2'
 import ProductDetailTable1 from './ProductDetailTable1'
 import ProductDetailTable2 from './ProductDetailTable2'
+import { useParams } from 'react-router-dom'
+import Cookies from 'js-cookie'
+import { useGetProductDetailQuery } from '../../../Redux/API/inventoryApi'
+import Loader from '../../Loader/Loader'
 
 const ProductDetail = () => {
+  const {id} = useParams();
+  const token = Cookies.get("token");
+  const {data , isLoading} = useGetProductDetailQuery({token,id});
+  console.log(data?.data);
+  const product = data?.data
   return (
     <MainLayout>
       <div className=' w-full flex justify-center'>
@@ -19,6 +28,8 @@ const ProductDetail = () => {
             btn2={"Product List"}
             button2={true}
             route={"/inventory/products"}/>
+          {isLoading ? <div><Loader/></div> : 
+
           <div className='flex gap-5'>
             {/* product detail   */}
             <div className=' w-[50%]'>
@@ -39,15 +50,15 @@ const ProductDetail = () => {
                         <div className= " flex justify-between w-full">
 
                             <div className='flex flex-col gap-3 ml-[213px]'>
-                                <h1 className=' text-xl text-[#fff] font-bold tracking-wider mb-1'>Blue Berry</h1>
+                                <h1 className=' text-xl text-[#fff] font-bold tracking-wider mb-1'>{product?.name}</h1>
                                 <div className=' flex flex-col gap-1 text-sm'>
                                   <div className='flex gap-1 tracking-wide font-medium'>
                                     <span className=' text-[#878787]'>Sale Price :</span>
-                                    <span className=' text-[#FFFFFF]'>10,000 MMK</span>
+                                    <span className=' text-[#FFFFFF]'>{product?.sale_price} {product?.unit}</span>
                                   </div>
                                   <div className='flex gap-1 tracking-wide font-medium'>
                                     <span className=' text-[#878787]'>Actual Price :</span>
-                                    <span className=' text-[#FFFFFF]'>8,000 MMK</span>
+                                    <span className=' text-[#FFFFFF]'>{product?.actual_price} {product?.unit}</span>
                                   </div>
                                 </div>
                             </div>
@@ -73,25 +84,25 @@ const ProductDetail = () => {
                   <div className={` pl-10 pt-6 pb-9 flex flex-col gap-5`}>
                         <p className=' flex items-center gap-14 text-[17px] tracking-wider font-medium'>
                           <span className=' text-[#878787] w-[150px]'>Name</span>
-                          <span className=' text-[#fff]'>: Bule Berry</span>
+                          <span className=' text-[#fff]'>: {product?.name}</span>
                         </p>
 
                         <p className=' flex items-center gap-14 text-[17px] tracking-wider font-medium'>
                           <span className=' text-[#878787] w-[150px]'>Brand</span>
-                          <span className=' text-[#fff]'>: Burma</span>
+                          <span className=' text-[#fff]'>: {product?.brand_name}</span>
                         </p>
                       
                         <p className=' flex items-center gap-14 text-[17px] tracking-wide font-medium'>
                           <span className=' text-[#878787] w-[150px]'>Stock</span>
-                          <span className=' text-[#fff]'>: 120</span>
+                          <span className=' text-[#fff]'>: {product?.total_stock}</span>
                         </p>
                         <p className=' flex items-center gap-14 text-[17px] tracking-wide font-medium'>
                           <span className=' text-[#878787] w-[150px]'>Unit</span>
-                          <span className=' text-[#fff]'>: MMK</span>
+                          <span className=' text-[#fff]'>: {product?.unit}</span>
                         </p>
                         <p className=' flex items-center gap-14 text-[17px] tracking-wide font-medium'>
                           <span className=' text-[#878787] w-[150px]'>More Information</span>
-                          <span className=' text-[#fff]'>: Fresh Fruits</span>
+                          <span className=' text-[#fff]'>: {product?.more_information}</span>
                         </p>
                   </div>
                 </div>
@@ -106,7 +117,7 @@ const ProductDetail = () => {
                 <ProductDetailTable2/>
               </div>
             </div>
-          </div>
+          </div>}
         </div>
       </div>
     </MainLayout>
