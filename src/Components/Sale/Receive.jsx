144b6d0b8f186134b2f7067c79201cd22eb54fb0
khinezinthin <khinezinthinwk1@gmail.com>
2023-08-26 16:10:52 +0630

    print style

--- src/Components/Sale/Receive.jsx
+++ src/Components/Sale/Receive.jsx
@@ -14,9 +14,9 @@ const Receive = () => {
     const printContent = document.getElementById('print-content');
     if (printContent) {
       // const originalContents = document.body.innerHTML;
-      const printContents = printContent.innerHTML;
+      // const printContents = printContent.innerHTML;
 
-      document.body.innerHTML = printContents;
+      // document.body.innerHTML = printContents;
       window.print();
 
       // document.body.innerHTML = originalContents;
@@ -26,7 +26,7 @@ const Receive = () => {
   return (
     <div className=" bg-[#202124]">
       {/* navbar  */}
-        <div className=' fixed w-full z-10 bg-[#202124]'>
+        <div className=' print:hidden fixed w-full z-10 bg-[#202124]'>
           <div className=" flex justify-between items-center border-b border-[#3f4245] text-[#e8eaed] px-5 py-3 b-60 w-full">
           <Link to={'/'}><h1 className=" font-semibold tracking-wider text-lg cursor-pointer">MMS</h1></Link>
                     <div className=" flex gap-5 items-center">
@@ -57,37 +57,37 @@ const Receive = () => {
       {/* receive  */}
       <div className=' pt-[130px] pb-10 flex justify-center items-center'>
         <div className=' w-[50%] bg-[#161618] px-5 pt-9 pb-10 text-[#e8eaed] receive-shadow rounded'>
-        <div id="print-content" className=' w-full'>
+        <div id="print-content" className=' w-full title'>
           <h1 className=' text-[25px] font-semibold px-8 pb-4 tracking-wide'>Receive</h1>
 
           <div>
             {/* voucher data  */}
             {receiveData?.records?.map(data => {
               return(
-                <div key={data?.id} className=' border-b pt-1 border-[#3f4245] flex justify-between items-center'>
+                <div key={data?.id} className=' border-bottom pt-1 flex justify-between items-center'>
                   <div className=' px-8 pb-2 flex flex-col'>
                     <p className=' tracking-wide text-lg'>{data?.product_name}</p>
                     <p className=' flex gap-2 tracking-wide text-[hsl(0,1%,67%)] '>
-                      <span className=''>{data?.quantity}</span>
-                      <span className=' text-end'>{data?.sale_price}</span>
+                      <span className='title'>{data?.quantity}</span>
+                      <span className=' text-end title'>{data?.sale_price}</span>
                     </p>
                   </div>
 
                   <div>
-                    <p className=' px-8 font-semibold text-xl tracking-wider'>{data?.cost}</p>
+                    <p className=' px-8 font-semibold text-xl tracking-wider title'>{data?.cost}</p>
                   </div>
                 </div>
               )
             })}
-
+            
             {/* total  */}
-            <div className=' flex justify-end mt-[100px] text-[#e8eaed]'>
+            <div className=' flex justify-end pt-1 total text-[#e8eaed] title'>
               <div className=' mx-8'>
               <p className='text-xl tracking-wide'>
                 <span>total-</span>
                 <span className=' font-semibold'>{receiveData?.net_total}</span>
               </p>
-              <p className='text-[hsl(0,1%,67%)] text-end tracking-wide'>Tax-{receiveData?.tax}</p>
+              <p className=' text-end tracking-wide tax-color'>Tax-{receiveData?.tax}</p>
               </div>
             </div>
           </div>
@@ -95,7 +95,7 @@ const Receive = () => {
         </div>
 
          {/* button  */}
-         <div className=' flex justify-center mt-8'>
+         <div className=' flex justify-center mt-8 print:hidden'>
               <button onClick={printHandler} className=' btn text-black w-[60%] tracking-wide'>
                 Print
               </button>
